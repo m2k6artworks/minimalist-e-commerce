fb1cc59446ef3cd46ceb89977e70c8522e1418c5
m2k6artworks <zidangibran14@gmail.com>
2020-10-09 21:53:26 +0700

    fix responsive nav, slider, & category

--- js/script.js
+++ js/script.js
@@ -71,7 +71,7 @@ const priceRangeOutput = qselect(".output");
 
 priceRange.addEventListener("input", (e) => {
   e.target.style.background = `linear-gradient(to right, #6558F5 0%, #6558F5 ${e.target.value}%, #fff ${e.target.value}%, #fff 100%)`;
-  priceRangeOutput.value = `$ ${e.target.value - 5} - $ ${e.target.value}`;
+  priceRangeOutput.value = `$${e.target.value - 5} - $${e.target.value}`;
 });
 
 let slider = tns({
